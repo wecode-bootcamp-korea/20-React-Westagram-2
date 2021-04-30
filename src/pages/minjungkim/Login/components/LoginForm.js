0dd78879bd4e9b62256645af3a95f65994de82be
Alice Kim <alice.inkorea.frontend@gmail.com>
2021-04-30 20:30:02 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/minjungkim/Login/components/LoginForm.js
+++ src/pages/minjungkim/Login/components/LoginForm.js
@@ -5,12 +5,24 @@ class LoginForm extends Component {
   state = {
     loginId: '',
     loginPassword: '',
+    // isBtnActive: '',
+    // isBtnDisabled: true,
   };
 
   handleValueChange = e => {
     this.setState({
       [e.target.name]: e.target.value,
     });
+  };
+
+  checkValidation = () => {
+    const reg = /^\d{3}[. -]?\d{3,4}[. -]?\d{4}$|[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9.]+|[a-zA-Z ]+/;
+
+    if (reg.test(this.state.loginId) && this.state.loginPassword.length > 5) {
+      console.log('wow');
+      return true;
+    }
+    return false;
   };
 
   goToMain = () => {
@@ -40,7 +52,12 @@ class LoginForm extends Component {
         />
         <span className="display_password">비밀번호 표시</span>
         <span className="hide_password">숨기기</span>
-        <button id="login_btn" onClick={this.goToMain}>
+        <button
+          className={this.checkValidation() && 'active'}
+          id="login_btn"
+          onClick={this.goToMain}
+          disabled={!this.checkValidation()}
+        >
           로그인
         </button>
       </div>
